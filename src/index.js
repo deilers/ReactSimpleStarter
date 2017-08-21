19add11c241d7137428b75827949c4a376e94733
Devon Eilers <devon.d.eilers@gmail.com>
2017-08-21 14:41:45 -0700

    search bar works but needs throttling

--- src/index.js
+++ src/index.js
@@ -19,7 +19,11 @@ class App extends Component {
             selectedVideo: null
          };
 
-        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+        this.videoSearch('surfboards');
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term}, (videos) => {
             this.setState({
                 videos,
                 selectedVideo: videos[0]
@@ -31,7 +35,7 @@ class App extends Component {
         
         return (
             <div>
-                <SearchBar/>
+                <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
                 <VideoDetail video={this.state.selectedVideo}/>
                 <VideoList 
                     onVideoSelect={selectedVideo => this.setState({selectedVideo})}
